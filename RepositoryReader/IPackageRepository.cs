4235ab480e4db15b9c615dcf6ff9a8a7fbe4811d
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-06-10 19:26:42 +0300

    Added package repository interface.

---
+++ RepositoryReader/IPackageRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace RepositoryReader
+{
+    public interface IPackageRepository
+    {
+								string Name { get; set; }				
+
+								string BaseUri { get; set; }
+
+								string GpgKeyUri { get; set; }
+
+        Task<IEnumerable<IPackageParameters>> Packages { get; set; }
+    }
+}
--- RepositoryReader/IRepositoryReader.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RepositoryReader
-{
-    public interface IRepositoryReader
-    {
-        Task<IEnumerable<IPackageParameters>> Packages { get; set; }
-    }
-}
